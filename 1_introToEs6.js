0ee1b55babac7c2ab88e3a1c025148ad1804edac
MAYANK PAL <145184894+programmer-mAYANK@users.noreply.github.com>
2025-11-12 18:29:51 +0530

    1_introToEs6

---
+++ 1_introToEs6.js
@@ -0,0 +1,9 @@
+// Ecmascript 6 - 2015 - advanced javascript - used in frameworks(React,Angular)
+// 1. variable declaration: let,const
+// 2. template/string literal - `string(variable/dynamic) string` - backtick
+// 3. Arrow function - function without 'function' keyword =>
+// 4. Spread operator/Rest parameter - ...arr, ...param
+// 5. Array methods: map(),filter(),reduce(),find(),...
+// 6. Destructing: array,object
+// 7. Class & object:
+// 8. asynchronus function: Promise,     
