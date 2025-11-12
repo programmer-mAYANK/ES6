b07dd651d39e601a9c234dd862e1d541e5b84cfa
MAYANK PAL <145184894+programmer-mAYANK@users.noreply.github.com>
2025-11-12 18:30:28 +0530

    2_arrowFunc.js

---
+++ 2_arrowFunc.js
@@ -0,0 +1,47 @@
+// Arrow function
+// single parameter
+// multiple parameter
+// no parameter passed
+
+//window.addEventListener("click",function(){})
+
+// window.addEventListener("click", () => {
+    //   alert("hello from arrow function")    
+// })
+
+// function greet() {
+//    console.log("Hello user")
+// }
+// let greet = () => {
+//    console.log("Hello user")
+// }
+// greet()
+
+// sum of two number
+// function sum(num1, num2){
+//    return num1 + num2;
+// }
+
+//let sumArr = (num1, num2) => {
+//    return num1 + num2;
+// }
+// console.log(sumArr(10, 20))
+
+// let sumArr = (num1 , num2) => num1 + num2;
+// console.log(sumArr(20, 30))
+
+// function double(num){
+//       return num * 2;
+// }
+
+// let double = num => num * 2;
+// console.log(double(5))
+
+// let square = num => num * num;
+// console.log(square(4))
+
+// reverse a string using arrow fuction -
+
+let string = "Mayank"
+let reverse = (str) => str.split('').reverse().join('')
+console.log()
